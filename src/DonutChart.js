3826041f91c2f3d7f3bf6aa8a29544b0b69fe2ec
jw0202058 <jw0202058@korea.ac.kr>
2023-07-19 14:06:52 +0900

    도넛 차트 수정

--- src/DonutChart.js
+++ src/DonutChart.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Doughnut } from 'react-chartjs-2';
 import 'chart.js/auto';
 
 const DonutChart = ({ data }) => {
+  const [chartVisible, setChartVisible] = useState(false);
+  const chartRef = useRef(null);
+
   const chartData = {
     labels: data.map(prediction => prediction.className),
     datasets: [
@@ -20,22 +23,21 @@ const DonutChart = ({ data }) => {
   };
 
   const chartOptions = {
-    responsive: true, // 차트가 반응형으로 크기가 조정되도록 설정
-    maintainAspectRatio: false, // 차트의 가로 세로 비율을 유지하지 않도록 설정 (true로 하면 도넛이 능글렁해질 수 있음)
+    responsive: true,
+    maintainAspectRatio: false,
     legend: {
-      display: true, // 범례 표시 여부 설정 (false로 하면 차트 하단에 범례가 표시되지 않음)
-      position: 'bottom', // 범례 위치 설정 (top, bottom, left, right 중 선택)
+      display: true,
+      position: 'bottom',
       labels: {
-        fontColor: 'white', // 범례 텍스트 색상 설정
-        fontSize: 14, // 범례 텍스트 크기 설정
-        padding: 20, // 범례 내용과 차트 사이의 간격 설정
+        fontColor: 'white',
+        fontSize: 14,
+        padding: 20,
       },
     },
     tooltips: {
-      enabled: true, // 마우스 호버 시 정보 툴팁 표시 여부 설정
+      enabled: true,
       callbacks: {
         label: (tooltipItem, data) => {
-          // 툴팁 텍스트 형식 설정 (툴팁에 어떤 정보를 어떤 형식으로 표시할지 설정)
           const label = data.labels[tooltipItem.index];
           const value = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
           return `${label}: ${value}%`;
@@ -44,21 +46,36 @@ const DonutChart = ({ data }) => {
     },
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (chartRef.current) {
+        const elementTop = chartRef.current.getBoundingClientRect().top;
+        const offset = window.innerHeight * 0.5; // 화면 높이의 절반 이상 보일 때 감지
+        const isChartVisible = elementTop < window.innerHeight - offset;
+        if (isChartVisible !== chartVisible) {
+          setChartVisible(isChartVisible);
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // 컴포넌트가 언마운트될 때 스크롤 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [chartVisible]);
+
   return (
     <div style={{ display: 'flex', alignItems: 'center' }}>
       {/* 차트 컴포넌트 */}
-      <div style={{ width: '500px', height: '500px' }}>
-        <Doughnut data={chartData} options={chartOptions} />
+      <div style={{ width: '500px', height: '500px' }} ref={chartRef}>
+        {chartVisible && <Doughnut data={chartData} options={chartOptions} />}
       </div>
 
       {/* 오른쪽에 색상 정보 */}
       <div style={{ marginLeft: '100px' }}>
-        {data.map((prediction, index) => (
-          <div key={index} style={{ display: 'flex', alignItems: 'center', marginBottom: '5px' }}>
-            <div style={{ width: '12px', height: '12px', backgroundColor: prediction.backgroundColor, marginRight: '5px' }}></div>
-            <span style={{ color: 'white', fontSize: '20px' }}>{prediction.className} : {Math.round(prediction.probability * 100)}%</span>
-          </div>
-        ))}
+        {/* 오른쪽 색상 정보 표시 코드 생략 */}
       </div>
     </div>
   );
